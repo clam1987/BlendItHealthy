c2335df6f9f7571d430f0c05450a77fdbd2b3c4c
Wilson Lam <lin-1987@live.com>
2020-01-07 21:57:23 -0800

    Got Images to wrap

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,31 @@ import Navbar from "./components/Nav/Nav";
 import Footer from "./components/Footer/Footer";
 import Banner from "./components/Banner/Banner";
 import homeBanner from "./assets/HomeBanner.json";
+import Image from "./components/Image/Image"
+import homeGallery from "./assets/homeGallery.json";
+import Wrapper from "./components/Wrapper/index";
+
+
+
+
 
 
 function App() {
   const Home = homeBanner.map(info => {
-   return <Banner key={info.id} h1={info.h1} message={info.message} image={require("./assets/images/" + info.image)} name={info.name} />
+    return <Banner key={info.id} h1={info.h1} message={info.message} image={require("./assets/images/" + info.image)} name={info.name} />
+  })
+  
+  const Gallery = homeGallery.map(image => {
+    return  <Image key={image.id} name={image.name} gallery={require("./assets/images/" + image.gallery)} />
   })
   return (
     <div className="App">
       <Navbar />
       {Home}
+      <Wrapper>
+      {Gallery}
+
+      </Wrapper>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -35,3 +50,4 @@ function App() {
 }
 
 export default App;
+
--- client/src/components/Image/Image.js
+++ client/src/components/Image/Image.js
@@ -1,2 +1,17 @@
 import React from "react";
 import "../Image/Image.css";
+
+let Image = (props) => {
+    // console.log(props)
+    return (
+        <div className="row row-cols-1 row-cols-md-3">
+      <div className="card-deck">
+        <div className="card">
+          <img src={props.gallery} className="card-img-top" alt={props.name}/>
+        </div>
+      </div>
+      </div>
+    )
+  }
+
+export default Image;
