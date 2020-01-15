2dd85cd0ac6c46fd80bfb9168b1565caa4816271
Wilson Lam <lin-1987@live.com>
2020-01-14 20:31:15 -0800

    uploaded images and will work on styling tomorrow

--- client/src/components/AboutBanner2/Banner2.js
+++ client/src/components/AboutBanner2/Banner2.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "../AboutBanner2/Banner2.css";
+import storeFront from "../../assets/images/store_front.png"
 
 let Banner2 = () => {
   return (
     <div className="container-fluid">
       <div className="row">
-        <div className="col-sm" id="banner2">
+        <div className="col-sm-8" id="banner2">
           <p className="aboutUs">About Us</p>
+        </div>
+        <div className="col-sm-4" id="bannerImg">
+            <img src={storeFront} alt="store_front" id="storeFront" />
         </div>
       </div>
       </div>
