80d6498e8063801822ca22813f486923a17af26a
Wilson Lam <lin-1987@live.com>
2020-01-16 20:24:27 -0800

    Updates

---
+++ precache-manifest.7672936cac529a581af1327278580041.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d7d58e0eca22efc2cd2daf91e08e172",
+    "url": "/BIH/index.html"
+  },
+  {
+    "revision": "831eaf42751a850103c9",
+    "url": "/BIH/static/css/main.882cfff5.chunk.css"
+  },
+  {
+    "revision": "d0e72b1c7de580f15497",
+    "url": "/BIH/static/js/2.3446e357.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/BIH/static/js/2.3446e357.chunk.js.LICENSE"
+  },
+  {
+    "revision": "831eaf42751a850103c9",
+    "url": "/BIH/static/js/main.109f1218.chunk.js"
+  },
+  {
+    "revision": "7aaab5140707c46f32bd",
+    "url": "/BIH/static/js/runtime-main.4652c88e.js"
+  },
+  {
+    "revision": "f1e39fb4f78d7c64847be33177746548",
+    "url": "/BIH/static/media/BIH.f1e39fb4.png"
+  },
+  {
+    "revision": "f1e39fb4f78d7c64847be33177746548",
+    "url": "/BIH/static/media/BIH1.f1e39fb4.png"
+  },
+  {
+    "revision": "38a7636de805285be354a2013512050d",
+    "url": "/BIH/static/media/BIH_logo.38a7636d.png"
+  },
+  {
+    "revision": "60d9048658257cb8f32bf3274cb875fc",
+    "url": "/BIH/static/media/BIH_logo2.60d90486.png"
+  },
+  {
+    "revision": "042e0ec24512437a0e08e81549d80acb",
+    "url": "/BIH/static/media/Headache Reliever2.042e0ec2.png"
+  },
+  {
+    "revision": "163d464b85ea032841305a6b46a9df35",
+    "url": "/BIH/static/media/Manach_5674.163d464b.png"
+  },
+  {
+    "revision": "99d4f1c4df237ddd907ee943273faa4d",
+    "url": "/BIH/static/media/Manach_56741.99d4f1c4.png"
+  },
+  {
+    "revision": "138724dff59d5d28dc7071bc323a691c",
+    "url": "/BIH/static/media/OurProgram.138724df.png"
+  },
+  {
+    "revision": "bc327d4907f377a141327016791f7d49",
+    "url": "/BIH/static/media/Playlist_Script.bc327d49.otf"
+  },
+  {
+    "revision": "08b7b580a50d93f82342b5b3cdb9fe74",
+    "url": "/BIH/static/media/TangyMango_5577.08b7b580.png"
+  },
+  {
+    "revision": "cf642694843d6cccc09e39ee65da9626",
+    "url": "/BIH/static/media/healthySmoothie.cf642694.png"
+  },
+  {
+    "revision": "c8dd442e0307725b3bfa0fe7469b9ebc",
+    "url": "/BIH/static/media/store_front.c8dd442e.png"
+  },
+  {
+    "revision": "fd9e7333e72ca3542a55d77e6f6a48dc",
+    "url": "/BIH/static/media/store_front1.fd9e7333.png"
+  },
+  {
+    "revision": "c9ae1a94cc4bd29dd5ceb7840ade255e",
+    "url": "/BIH/static/media/store_inside.c9ae1a94.png"
+  },
+  {
+    "revision": "f5f5101b34c7c4512c2a6bd2cdcb2855",
+    "url": "/BIH/static/media/store_inside1.f5f5101b.png"
+  },
+  {
+    "revision": "81263a29a6c258ee507de8f665dfab41",
+    "url": "/BIH/static/media/tk.81263a29.png"
+  }
+]);
