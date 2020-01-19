cf6acf2ba7f4cf44ff1da342d46ef9666096d684
Wilson Lam <lin-1987@live.com>
2020-01-18 20:16:17 -0800

    Finished working buttons and fixed margins for menu's page

--- client/src/pages/Menu/Menu.js
+++ client/src/pages/Menu/Menu.js
@@ -1,86 +1,172 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Route, Router, Switch } from "react-router-dom";
 import BannerReusable from "../../components/BannerReusable/BannerReuseable"
 import "../Menu/Menu.css";
+import redMenu from "../../assets/images/red_menu.png"
 
 let Menu = () => {
-    let button = document.getElementsByClassName("menuBtn")
-    // console.log(button)
-    let [clicked, setClicked] = useState([
-       {
+    let initialRedState = {
+        dataColor: "",
+        isClicked: false,
+        pageStyle: {
+          backgroundColor:"#9AB49D",
+          fontSize: 24
+        }
+ };
+
+    let initialGreenState = {
+        dataColor: "",
+        isClicked: false,
+        pageStyle: {
+          backgroundColor:"#9AB49D",
+          fontSize: 26
+        }
+ };
+
+    let initialBlueState = {
+        dataColor: "",
+        isClicked: false,
+        pageStyle: {
+          backgroundColor:"#9AB49D",
+          fontSize: 20
+        }
+ };
+
+    let initialPurpleState = {
+        dataColor: "",
+        isClicked: false,
+        pageStyle: {
+          backgroundColor:"#9AB49D",
+          fontSize: 26
+        }
+ };
+    let initialYellowState = {
+        dataColor: "",
+        isClicked: false,
+        pageStyle: {
+          backgroundColor:"#9AB49D",
+          fontSize: 32
+        }
+ };
+    let [clickedRed, setClickedRed] = useState({
            dataColor: "",
-           isClicked: false
-       }
-    ])
-    
-    let handleChange = (e) => {
-        // console.log(e.target.dataset.color);
-        // console.log(e);
+           isClicked: false,
+           pageStyle: {
+             backgroundColor:"#9AB49D",
+             fontSize: 24
+           }
+    });
+
+    let [clickedGreen, setClickedGreen] = useState({
+        dataColor: "",
+        isClicked: false,
+        pageStyle: {
+          backgroundColor:"#9AB49D",
+          fontSize: 26
+        }
+ });
+
+    let [clickedBlue, setClickedBlue] = useState({
+        dataColor: "",
+        isClicked: false,
+        pageStyle: {
+          backgroundColor:"#9AB49D",
+          fontSize: 20
+        }
+ });
+ let [clickedPurple, setClickedPurple] = useState({
+    dataColor: "",
+    isClicked: false,
+    pageStyle: {
+      backgroundColor:"#9AB49D",
+      fontSize: 26
     }
-    
+});
+let [clickedYellow, setClickedYellow] = useState({
+    dataColor: "",
+    isClicked: false,
+    pageStyle: {
+      backgroundColor:"#9AB49D",
+      fontSize: 32
+    }
+});
+
     let handleClick = (e) => {
         console.log("click me")
         let val = e.target.dataset.color
-        // console.log(val)
-        let style;
 
 
         // console.log(clicked)
         switch (val) {
             case "red":
-                return setClicked({ dataColor: val, isClicked: true });
-                // console.log("hello");
-                break;
+                setClickedGreen(initialGreenState);
+                setClickedBlue(initialBlueState);
+                setClickedPurple(initialPurpleState);
+                setClickedYellow(initialYellowState);
+                return setClickedRed({ dataColor: val, isClicked: true, pageStyle: { backgroundColor:"#E55538", fontSize:24 } });
             case "green":
-                setClicked({ dataColor: val, isClicked: true });
-                console.log("world");
-                break;
+                setClickedRed(initialRedState);
+                setClickedBlue(initialBlueState);
+                setClickedPurple(initialPurpleState);
+                setClickedYellow(initialYellowState);
+                return setClickedGreen({ dataColor: val, isClicked: true, pageStyle: { backgroundColor:"#11AC4C", fontSize:26 } });
+            case "blue":
+                setClickedRed(initialRedState);
+                setClickedGreen(initialGreenState);
+                setClickedPurple(initialPurpleState);
+                setClickedYellow(initialYellowState);
+                return setClickedBlue({ dataColor: val, isClicked: true, pageStyle: { backgroundColor:"#0093D0", fontSize:20 } })
+            case "purple":
+                setClickedRed(initialRedState);
+                setClickedGreen(initialGreenState);
+                setClickedBlue(initialBlueState);
+                setClickedYellow(initialYellowState);
+                return setClickedPurple({ dataColor: val, isClicked: true, pageStyle: { backgroundColor:"#D0336A", fontSize:26 } })
+            case "yellow":
+                setClickedRed(initialRedState);
+                setClickedGreen(initialGreenState);
+                setClickedBlue(initialBlueState);
+                setClickedPurple(initialPurpleState);
+                return setClickedYellow({ dataColor: val, isClicked: true, pageStyle: { backgroundColor:"#FEB600", fontSize:32 } })
             default:
-                console.log("disgunasuk");
-                break;
+                return
         }
-
-            // switch (clicked.isClicked === true) {
-            //     case "red":
-            //         console.log("hello");
-            //         break;
-            //     case clicked[1]:
-            //         console.log("world");
-            //     case "blue":
-            //         console.log("1234");
-            //     case "purple":
-            //         console.log("5678");
-            //     case "yellow":
-            //         console.log("1987");
-            //     default:
-            //         console.log("In It to win it")
-            // }
-
-     
-            
-
-
-    }
+    };
     // console.log(clicked);
             return (
                 <>
         <BannerReusable text="Our Menu"/>
-        <div className="row">
+        <div className="row" id="rowMenuBtn">
             <div className="col-sm-1"></div>
             <div className="col-sm-2">
-            <button className="btn menuBtn" data-color="red" onClick={handleClick} style={clicked.isClicked ? { backgroundColor:"#E55538", fontSize:24 } : { backgroundColor:"#9AB49D", fontSize:24 } }>ALL FRUITS, FRUITS & VEGGIES, MEAL REPLACER</button>
+            <button className="btn menuBtn" data-color="red" onClick={handleClick} style={clickedRed.pageStyle}>ALL FRUITS, FRUITS & VEGGIES, MEAL REPLACER</button>
             </div>
             <div className="col-sm-2">
-            <button className="btn menuBtn" data-color="green" onClick={handleClick} style={clicked.isClicked ? { fontSize:26, backgroundColor:"#11AC4C" } : { fontSize:26, backgroundColor:"#9AB49D" } }>CLEANSING DETOX, NATURAL REMEDIES</button>
+            <button className="btn menuBtn" data-color="green" onClick={handleClick} style={clickedGreen.pageStyle}>CLEANSING DETOX, NATURAL REMEDIES</button>
             </div>
             <div className="col-sm-2">
-            <button className="btn menuBtn" style={{fontSize:24, backgroundColor:"#0093D0"}}>WORKOUT SMOOTHIES, COFFEE SMOOTHIES, HEALTHY SNACKS</button>
+            <button className="btn menuBtn" data-color="blue" onClick={handleClick} style={clickedBlue.pageStyle}>WORKOUT SMOOTHIES, COFFEE SMOOTHIES, HEALTHY SNACKS</button>
             </div>
             <div className="col-sm-2">
-            <button className="btn menuBtn" style={{fontSize:26, backgroundColor:"#D0336A"}}>ORGANIC TEA, ORGANIC MATCHA</button>
+            <button className="btn menuBtn" data-color="purple" onClick={handleClick} style={clickedPurple.pageStyle}>ORGANIC TEA, ORGANIC MATCHA</button>
             </div>
             <div className="col-sm-2">
-            <button className="btn menuBtn" style={{fontSize:32, backgroundColor:"#FEB600"}}>BUILD YOUR OWN</button>
+            <button className="btn menuBtn" data-color="yellow" onClick={handleClick} style={clickedYellow.pageStyle}>BUILD YOUR OWN</button>
+            </div>
+            <div className="col-sm-1"></div>
+        </div>
+        <div className="row">
+            <div className="col-sm-1"></div>
+            <div className="col-sm-10">
+                {/* pseudo code:
+                render image based on data-color and clicked state
+                create a handleChange function that
+                make a json file of images
+                1. changes images based on the data-color
+                2. must check to see if isClicked = true
+                    2a. call current color state i.e. redstate and run a check to see if isClicked = true
+                    2b if is true, render image in img src */}
+                <img src="placeholder" alt="placeholder" />
             </div>
             <div className="col-sm-1"></div>
         </div>
