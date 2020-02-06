4b93f20f85a30763ff9aa8a501f49e2cff1a3051
Wilson Lam <lin-1987@live.com>
2020-02-05 21:01:47 -0800

    added slide package

--- src/pages/Testimonials/Testimonials.js
+++ src/pages/Testimonials/Testimonials.js
@@ -1,72 +1,137 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../Testimonials/Testimonials.css";
-import { CSSTransition } from "react-transition-group"
+import { CSSTransition, TransitionGroup } from "react-transition-group"
+import { Slide } from "react-slideshow-image";
+
 
 let Testimonials = () => {
-  const [inProp, setInProp] = useState(false);
+
   return (
     <>
-      <Carousel />
-      <h1>Hello World</h1>
+{/* <Carousel items={items} active={0}/> */}
+<Slideshow />
+    <h1>Hello World</h1>
     </>
   );
 };
 
 export default Testimonials;
 
-let Carousel = props => {
-  const [run, setRun] = useState({
-    items: props.items,
-    active: props.active,
-    direction: ""
-  })
+const Slideshow = () => {
+  const slides = ['https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/paris.jpg', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/singapore.jpg', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/prague.jpg']
+  const slidess = [
+    {
+      city: 'Paris',
+      country: 'France',
+      img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/paris.jpg',
+    },
+    {
+      city: 'Singapore',
+      img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/singapore.jpg',
+    },
+    {
+      city: 'Prague',
+      country: 'Czech Republic',
+      img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/prague.jpg',
+    },
+    {
+      city: 'Amsterdam',
+      country: 'Netherlands',
+      img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/amsterdam.jpg',
+    },
+    {
+      city: 'Moscow',
+      country: 'Russia',
+      img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/moscow.jpg',
+    },
+  ];
 
-let moveLeft = () => {
-  let newActive = run.active;
-  newActive--;
-  setRun({...run, active: newActive < 0 ? run.items.length - 1 : newActive, direction: "left"})
-}
-
-let moveRight = () => {
-  let newActive = run.active
-  setRun({...run, active: (newActive + 1) % run.items.length, direction: "right"})
-}
-
-let generateItems = () => {
-  let items = [];
-  let level;
-  console.log(run.active);
-  for (let i = run.active - 2; i < run.active + 3; i++) {
-    let index = i
-    if (i < 0) {
-      index = i % run.items.length + i;
-    } else if (i >= run.items.length) {
-      index = i % run.items.length
+  const properties = {
+    duration: 5000,
+    transitionDuration: 500,
+    infinite: true,
+    indicators: true,
+    arrows: true,
+    onChange: (oldIndex, newIndex) => {
+      console.log(`slide transition from ${oldIndex} to ${newIndex}`);
     }
-    level = run.active - i;
-    items.push(<Item key={index} id={run.items[i]} level={level} />)
   }
-  return items
-}
-
   return (
-    <div id="carousel" className="noselect">
-      <div className="arrow arrow-left" onClick={moveLeft}><i className="fi-arrow-left"></i></div>
-      <CSSTransition transitionName={run.direction}>
-        {generateItems()}
-      </CSSTransition>
-      <div className="arrow arrow-right" onClick={moveRight}><i className="fi-arrow-right"></i></div>
+    <div className="slide-container">
+       <Slide {...properties}>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slides[0]})`}}>
+              <span>Slide 1</span>
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slides[1]})`}}>
+              <span>Slide 2</span>
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slides[2]})`}}>
+              <span>Slide 3</span>
+            </div>
+          </div>
+        </Slide>
     </div>
   )
 }
 
-let Item = (props) => {
-  const [curLVL, setCurLVL] = useState({level: props.level});
-  const className= `item level ${curLVL.level}`
+// let Carousel = props => {
+//   const [run, setRun] = useState({
+//     items: props.items,
+//     active: props.active,
+//     direction: ""
+//   })
 
-  return (
-    <div className={className}>
-      {props.id}
-    </div>
-  )
-}
+// let moveLeft = () => {
+//   let newActive = run.active;
+//   newActive--;
+//   setRun({...run, active: newActive < 0 ? run.items.length - 1 : newActive, direction: "left"})
+// }
+
+// let moveRight = () => {
+//   let newActive = run.active
+//   setRun({...run, active: (newActive + 1) % run.items.length, direction: "right"})
+// }
+
+// let generateItems = () => {
+//   let items = [];
+//   let level;
+//   console.log(run.active);
+//   for (let i = run.active - 2; i < run.active + 3; i++) {
+//     let index = i
+//     if (i < 0) {
+//       index = i % run.items.length + i;
+//     } else if (i >= run.items.length) {
+//       index = i % run.items.length
+//     }
+//     level = run.active - i;
+//     items.push(<Item key={index} id={run.items[i]} level={level} />)
+//   }
+//   return items;
+// }
+
+//   return (
+//     <div id="carousel" className="noselect">
+//       <div className="arrow arrow-left" onClick={moveLeft}><i class="fas fa-arrow-circle-left"></i></div>
+//       <CSSTransition transitionName={run.direction}>
+//         {generateItems}
+//       </CSSTransition>
+//       <div className="arrow arrow-right" onClick={moveRight}><i class="fas fa-arrow-alt-circle-right"></i></div>
+//     </div>
+//   )
+// }
+
+// let Item = (props) => {
+//   const [curLVL, setCurLVL] = useState({level: props.level});
+//   const className= `item level ${curLVL.level}`
+
+//   return (
+//     <div className={className}>
+//       {props.id}
+//     </div>
+//   )
+// }
